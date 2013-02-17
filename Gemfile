b4e568707e8a8292bc54eed8ae1c634382007f39
Thorbjørn Hermansen <thhermansen@gmail.com>
2013-02-17 18:14:28 +0100

    Rails added.

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'jquery-rails'
+gem 'sqlite3'
+
+gem 'ember-rails', github: 'emberjs/ember-rails'
+gem 'active_model_serializers'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Things::Application.config.secret_token = '550690f43f4481abf6bd0f0972c56a2952494c0e196a2c207c198044b93ee6ebbf8aef26d978b42d935f727e9cee8a910fe95424cb10690c17051ea9433b6e64'
